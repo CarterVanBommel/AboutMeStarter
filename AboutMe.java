916142a84bd43c36911dafc87177a4b5fd4be712
CarterVanBommel <71711972+CarterVanBommel@users.noreply.github.com>
2020-09-22 14:14:00 -0400

    Did the required work.

--- AboutMe.java
+++ AboutMe.java
@@ -1,24 +1,32 @@
 /*
- * Programmer:
- * Date:
- * Purpose:
+ * Programmer: Carter Van Bommel
+ * Date: September 22, 2020
+ * Purpose: To demonstrate the  
  */
 
 public class AboutMe {
-    public static void main (String [] args) {
-        // Declaring the variables
-        // TODO: Write the code the declares the variables
 
+	public static void main(String[] args) {
+
+		// Declaring the variables
+		final String MY_NAME = "Carter";
+		int myAge = 16;
+		double myHeightInMetres = 1.78;
+		
         // Print the values
-        System.out.println("Hi, my name is " + myName);
+        System.out.println("Hi, my name is " + MY_NAME + ".");
         System.out.println("I am " + myAge + " years old.");
         System.out.println("My height is " + myHeightInMetres + " m.");
 
         // Change the variables
-        // TODO: Write the code that changes the variables
+        myAge = 18;
+        myHeightInMetres = 1.83;
 
         // Print the new values
-        System.out.println("Some time passes...");
-        // TODO: Write the last two print statements
-    }
+		System.out.println("Some time passes...");
+		System.out.println("I am now " + myAge + " years old.");
+		System.out.println("I have grown and am " + myHeightInMetres + " m tall.");
+				
+	}
+
 }
